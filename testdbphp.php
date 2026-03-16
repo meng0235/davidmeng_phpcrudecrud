af92278fd475900b9df9d3266763abbcfdf4f886
David Meng <meng0235@umn.edu>
2026-03-16 06:56:20 +0000

    Initial commit

---
+++ testdbphp.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Northland Analytics PHP/MySQL Test Page</title>
+</head>
+
+<body>
+
+<h1>Northland Analytics Php/MySQL Test Page</h1>
+
+<?php
+
+$conn = new mysqli('local
+
